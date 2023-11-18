3980ed9fde0b8e32e058aa19a8568ae86e1923a9
Jhfnetboy(Survivor) <jhfnetboy@gmail.com>
2023-11-18 18:52:00 +0300

    env config

--- main.go
+++ main.go
@@ -3,27 +3,27 @@ package main
 import (
 	"Adapters/conf"
 	"Adapters/infra"
-	"fmt"
 
+	//  "fmt"
 	"github.com/totoval/framework/helpers/zone"
-	"go.bug.st/serial"
+	// "go.bug.st/serial"
 )
 
 func main() {
- getCommlist2()
- port, baud := conf.GetSim800c()
- if c, err := infra.NewSim800c(port, baud, 5*zone.Second); err == nil {
-  gw := infra.New(c)
+	//  getCommlist2()
+	port, baud := conf.GetSim800c()
+	if c, err := infra.NewSim800c(port, baud, 5*zone.Second); err == nil {
+		gw := infra.New(c)
 
-  gw.Listen()
- }
+		gw.Listen()
+	}
 }
 
-func getCommlist2() {
- ports, _ := serial.GetPortsList()
+// func getCommlist2() {
+// 	ports, _ := serial.GetPortsList()
 
- fmt.Printf("%#v", ports)
- for _, port := range ports {
-  fmt.Printf("Find serial port: %v\n", port)
- }
-}
+// 	fmt.Printf("%#v", ports)
+// 	for _, port := range ports {
+// 		fmt.Printf("Find serial port: %v\n", port)
+// 	}
+// }
