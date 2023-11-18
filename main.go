c7a2ffe8478e95809cbbb3f2a296e9e1ff95801f
Jhfnetboy(Survivor) <jhfnetboy@gmail.com>
2023-11-18 18:25:40 +0300

    add conf and show local port

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-        "AirAccountSmsAdapter/conf"
-        "AirAccountSmsAdapter/infra"
+	"AirAccountSmsAdapter/conf"
+	"AirAccountSmsAdapter/infra"
+	"fmt"
+
+	"github.com/totoval/framework/helpers/zone"
+	"go.bug.st/serial"
 )
 
 func main() {
-        port, baud := conf.GetSim800c()
-        }
+ getCommlist2()
+ port, baud := conf.GetSim800c()
+ if c, err := infra.NewSim800c(port, baud, 5*zone.Second); err == nil {
+  gw := infra.New(c)
+
+  gw.Listen()
+ }
+}
+
+func getCommlist2() {
+ ports, _ := serial.GetPortsList()
+
+ fmt.Printf("%#v", ports)
+ for _, port := range ports {
+  fmt.Printf("找到串口: %v\n", port)
+ }
 }
